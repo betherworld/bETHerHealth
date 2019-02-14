3bce36e772b079514ec3b0f7eaf1fb747500aa2c
luschmi <47520966+luschmi@users.noreply.github.com>
2019-02-15 00:18:56 +0100

    Update Version2_main.sol

--- Version2_main.sol
+++ Version2_main.sol
@@ -3,9 +3,9 @@ pragma solidity >=0.4.22 <0.6.0;
 //Reward System given in other contracts
 
 contract insurance {
-        function getReward(/*uint _tier,*/ uint _id) public view returns (string);
-        function addRep(uint _rep) public;
-        function getRep() public view returns (uint) ;
+       function addRep(uint _rep) public;
+       function getRep() public view returns (uint);
+       function getReward(uint _id) public returns (string, uint);
     // function getConversionRate() public view returns (uint);
     // uniform ^
     
@@ -34,19 +34,20 @@ contract bETHerHealth {
     
     function createRunner(uint _insuranceID, uint _daysUntilExpiry) public {
         // uint[] memory arr;
-        users[msg.sender] =user(_insuranceID,0,0, now + 365 days);//, arr);
+        users[msg.sender] =user(_insuranceID,0,0, now + _daysUntilExpiry*60*60*24);//, arr);
         
     }
     
-    /*
-    function getInsurance() public view returns (uint){
-        return users[msg.sender].insuranceID;
-    }
     
+    /*
     function getAddressFromID(uint _id) public view returns (address){
         return insuranceAddress[_id];
     }
     */
+    function getInsuranceRepFromID(uint _id) public view returns (uint){
+        return insurance(insuranceAddress[_id]).getRep();
+    }
+    
     function setInsurance(uint _insuranceID) public {
         users[msg.sender].insuranceID = _insuranceID;
     }
@@ -55,25 +56,29 @@ contract bETHerHealth {
         return insurance(insuranceAddress[_id]).getRep();
     }
     */
-    function addInsuranceRep(uint _amount) private returns (uint){
+    function addInsuranceRep(uint _amount) private returns (bool){
         uint _id = users[msg.sender].insuranceID;
         insurance i = insurance(insuranceAddress[_id]);
         i.addRep(_amount);
     }
     
-    function redeemCoins(uint _distance, uint _age, uint _sport) public returns (uint){
+    
+    function redeemCoins(uint _distance, uint _age, uint _sport) public returns (bool){
        // uint myInsurance = users[msg.sender].insuranceID;
         uint earned = _distance * _age * _sport;
         users[msg.sender].lifetimeCoins += earned;
         users[msg.sender].coins += earned;
         
-        addInsuranceRep(earned);
+       addInsuranceRep(earned);
     }
     
     function getCurrentCoins() public view returns (uint){
         return users[msg.sender].coins;
     }
     
+    function getLifetimeCoins() public view returns (uint){
+        return users[msg.sender].lifetimeCoins;
+    }
     
     function newInsuranceAt(address _add) external returns (uint){
         uint _insuranceID = insuranceCount;
@@ -87,18 +92,20 @@ contract bETHerHealth {
     }
     */
     
-    
-    
     function deleteUser(address _remove) public {
         users[_remove] = users[0];
     }
     
-    function getRewards(uint _rewardID) public constant returns (string){
+    function getRewards(uint _rewardID) public returns (string){
         require(now<users[msg.sender].expiryDate);
-        
+        require(users[msg.sender].coins > cost);
         uint _id = users[msg.sender].insuranceID;
         insurance _i = insurance(insuranceAddress[_id]);
-        return _i.getReward(_rewardID);//_rewardTier,_rewardID);
+        uint cost;
+        string memory text;
+        (text,cost) = _i.getReward(_rewardID);
+        users[msg.sender].coins -= cost;
+        return text;//_rewardTier,_rewardID);
     }
   /*  
     function claimReward(uint _rewardTier,uint _rewardID) public returns (string){
